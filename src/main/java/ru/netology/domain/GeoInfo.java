805c8b02c84159a531f262db8b96b2736b3ff9a1
Marina <marina-insightful@mail.ru>
2020-10-07 19:52:10 +0300

    add Post and Action for push with tag v1

--- src/main/java/ru/netology/domain/GeoInfo.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.domain;
-
-public class GeoInfo {
-    private String typ; // тип места
-    private String coordinates; // координаты места
-    private PlaceInfo place; // описание места (если оно добавлено)
-}
--- src/main/java/ru/netology/domain/RepostsInfo.java
+++
@@ -1,6 +0,0 @@
-package ru.netology.domain;
-
-public class RepostsInfo {
-    private int count; // число пользователей, скопировавших запись
-    private int[] userReposted  = { 0 , 1 }; // наличие репоста от текущего пользователя (1 — есть, 0 — нет)
-}
